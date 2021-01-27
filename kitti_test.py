61b6a04e5866ca3a81dca82659271d5c239e3e61
alexchung <yonganzhong@outlook.com>
2021-01-27 13:51:46 +0800

    update code

--- kitti_test.py
+++ kitti_test.py
@@ -15,19 +15,23 @@ import mmcv
 
 from mmdet.datasets import build_dataset
 from mmdet.apis import init_detector, inference_detector, show_result_pyplot
+from mmcv import Config
+from mmdet.apis import set_random_seed
 
 from kitti_config import update_config
 
+
 cfg = update_config()
+# cfg = Config.fromfile('./configs/kitti/cascade_r50_fpn_1x.py')
 
 dataset = [build_dataset(cfg.data.train)]
 
 def test():
     checkpoint = osp.join(cfg.work_dir, 'epoch_12.pth')
-    img = mmcv.imread(osp.join('./data/kitti_tiny/training/image_2', '000068.jpeg'))
+    img = mmcv.imread(osp.join('./data/kitti_tiny/training/image_2', '000015.jpeg'))
 
     model = init_detector(config=cfg, checkpoint=checkpoint, device='cuda:0')
-
+    print(model)
     # Add an attribute for visualization convenience
     model.CLASSES = dataset[0].CLASSES
 
